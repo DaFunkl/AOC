6a3813cad0d8597886d6017621848aa767bb4bf9
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-01-06 10:20:33 +0100

    qs

--- src/main/java/de/monx/aoc/year15/Y15D19.java
+++ src/main/java/de/monx/aoc/year15/Y15D19.java
@@ -1,19 +1,97 @@
 package de.monx.aoc.year15;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import de.monx.aoc.util.Day;
 
 public class Y15D19 extends Day {
+	String molecule = "";
+	Map<String, List<String>> replacements = new HashMap<>();
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		parse();
+		char[] msg = molecule.toCharArray();
+		Character pC = null;
+		Set<String> set = new HashSet<>();
+		String pStr = "";
+		String ppStr = "";
+		String key = "";
+		String key2 = "";
+		for (int i = 0; i < msg.length; i++) {
+			char c = msg[i];
+			key = (pC != null ? pC + "" : "") + c;
+			key2 = c + "";
+			if (replacements.containsKey(key)) {
+				for (String s : replacements.get(key)) {
+					String pre = key.length() == 2 ? ppStr : pStr;
+					set.add(pre + s + molecule.substring(i + 1));
+				}
+			} else if (replacements.containsKey(key2)) {
+				for (String s : replacements.get(key2)) {
+					set.add(pStr + s + molecule.substring(i + 1));
+				}
+			} else {
+				pC = c;
+			}
+			pStr += c;
+			ppStr += i >= 1 ? msg[i - 1] : "";
+		}
+		return set.size();
 	}
 
 	@Override
 	public Object part2() {
-		
+		String msg = molecule;
+		String prev = "";
+		int count = 0;
+		while (!msg.equals("e") && !prev.equals(msg)) {
+			prev = new String(msg);
+			for (String k : replacements.keySet()) {
+				for (String r : replacements.get(k)) {
+					msg = msg.replace(r, k);
+					
+					Pattern p = Pattern.compile(r, Pattern.LITERAL);
+					Matcher m = p.matcher(outtext);
+
+					int counter = 0;
+					StringBuffer sb = new StringBuffer();
+					while (m.find()) {
+					    counter++;
+					    m.appendReplacement(sb, repto);
+					}
+					m.appendTail(sb);
+					
+				}
+			}
+		}
 		return null;
 	}
 
+	void parse() {
+		boolean msg = false;
+		for (String s : getInputList()) {
+			if (s.isBlank()) {
+				msg = true;
+				continue;
+			}
+			if (msg) {
+				molecule = s;
+			} else {
+				var spl = s.split(" => ");
+				if (!replacements.containsKey(spl[0])) {
+					List<String> t = new ArrayList<>();
+					replacements.put(spl[0], t);
+				}
+				replacements.get(spl[0]).add(spl[1]);
+			}
+		}
+	}
 }
