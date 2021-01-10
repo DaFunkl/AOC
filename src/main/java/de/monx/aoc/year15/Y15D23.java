4ef81812a6eb885ba9b56adb429b91a97d3c1a57
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-01-10 12:09:53 +0100

    y15d23

--- src/main/java/de/monx/aoc/year15/Y15D23.java
+++ src/main/java/de/monx/aoc/year15/Y15D23.java
@@ -1,6 +1,8 @@
 package de.monx.aoc.year15;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import de.monx.aoc.util.Day;
 
@@ -10,16 +12,32 @@ public class Y15D23 extends Day {
 
 	@Override
 	public Object part1() {
-		int a = 0;
-		int b = 0;
-
-		return null;
+		return solve(0, 0);
 	}
 
 	@Override
 	public Object part2() {
-
-		return null;
+		return solve(1, 0);
 	}
 
+	int solve(int a, int b) {
+		Map<String, Integer> regs = new HashMap<>();
+		regs.put("a", a);
+		regs.put("b", b);
+
+		for (int ip = 0; ip < opCode.size(); ip++) {
+			var opc = opCode.get(ip);
+			var op = opc.replace(", ", " ").split(" ");
+			switch (op[0]) { //@formatter:off
+				case "hlf": regs.put(op[1], regs.get(op[1]) >> 1); break;
+				case "tpl": regs.put(op[1], regs.get(op[1]) * 3); break;
+				case "inc": regs.put(op[1], regs.get(op[1]) + 1); break;
+				case "jmp": ip += Integer.valueOf(op[1]) - 1; break;
+				case "jie": if (regs.get(op[1]) % 2 == 0) ip += Integer.valueOf(op[2]) - 1; break;
+				case "jio": if (regs.get(op[1]) == 1) ip += Integer.valueOf(op[2]) - 1; break;
+				default: System.err.println("Unkown opCode[" + op[0] + "] -> " + opc); break;
+				}//@formatter:on
+		}
+		return regs.get("b");
+	}
 }
