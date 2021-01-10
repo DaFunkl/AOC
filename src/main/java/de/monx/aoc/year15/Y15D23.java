a1664b437603a0ab9660f2397fe8fd623cdac45e
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-01-10 10:32:53 +0100

    y15d22

--- src/main/java/de/monx/aoc/year15/Y15D23.java
+++ src/main/java/de/monx/aoc/year15/Y15D23.java
@@ -1,18 +1,24 @@
 package de.monx.aoc.year15;
 
+import java.util.List;
+
 import de.monx.aoc.util.Day;
 
 public class Y15D23 extends Day {
 
+	List<String> opCode = getInputList();
+
 	@Override
 	public Object part1() {
-		
+		int a = 0;
+		int b = 0;
+
 		return null;
 	}
 
 	@Override
 	public Object part2() {
-		
+
 		return null;
 	}
 
