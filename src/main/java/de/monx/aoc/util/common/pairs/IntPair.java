391db1a2ae66c3af35285c5cad4e8eb6ae1e2682
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-01-10 22:56:20 +0100

    qs

--- src/main/java/de/monx/aoc/util/common/pairs/IntPair.java
+++ src/main/java/de/monx/aoc/util/common/pairs/IntPair.java
@@ -11,4 +11,22 @@ public class IntPair extends Pair<Integer, Integer> {
 	public IntPair add(IntPair o) {
 		return new IntPair(o.first + this.first, o.second + this.second);
 	}
+
+	public IntPair add(IntPair o, int a) {
+		return new IntPair(o.first * a + this.first, o.second * a + this.second);
+	}
+
+	public void addi(IntPair o) {
+		this.first += o.first;
+		this.second += o.second;
+	}
+
+	public void addi(IntPair o, int a) {
+		this.first += o.first * a;
+		this.second += o.second * a;
+	}
+
+	public int manhattenDistance() {
+		return Math.abs(first) + Math.abs(second);
+	}
 }
--- src/main/java/de/monx/aoc/year16/Y16D1.java
+++ src/main/java/de/monx/aoc/year16/Y16D1.java
@@ -1,19 +1,42 @@
 package de.monx.aoc.year16;
 
-import de.monx.aoc.util.Day;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import de.monx.aoc.util.Day;
+import de.monx.aoc.util.common.Pair;
+import de.monx.aoc.util.common.pairs.IntPair;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class Y16D1 extends Day {
+
+	List<Pair<Boolean, Integer>> data = Arrays.stream(getInputString().split(", "))//
+			.map(x -> new Pair(x.charAt(0) == 'L', Integer.valueOf(x.charAt(1) + ""))) //
+			.collect(Collectors.toList());
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		IntPair dir = new IntPair(0, -1);
+		IntPair pos = new IntPair(0, 0);
+		System.out.println(pos + " <-> " + dir);
+		for (var d : data) {
+			dir = turn(dir, d.first);
+			pos.addi(dir, d.second);
+		}
+		return pos.manhattenDistance();
 	}
 
 	@Override
 	public Object part2() {
-		
 		return null;
 	}
 
+	IntPair turn(IntPair dir, boolean l) {
+		if (l) {
+			return new IntPair(dir.second, dir.first * -1);
+		} else {
+			return new IntPair(dir.second * -1, dir.first);
+		}
+	}
 }
