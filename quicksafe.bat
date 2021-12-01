8fb0e5bcb6531a240f78661fadea094092036aa7
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-12-01 07:26:02 +0100

    quicksafe

---
+++ quicksafe.bat
@@ -0,0 +1,3 @@
+git add * 
+git commit -m "quicksafe"
+git push
--- src/main/java/de/monx/aoc/year21/Y21D01.java
+++ src/main/java/de/monx/aoc/year21/Y21D01.java
@@ -1,18 +1,38 @@
 package de.monx.aoc.year21;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import de.monx.aoc.util.Day;
 
 public class Y21D01 extends Day {
+	List<Integer> in = new ArrayList<>();
 
 	@Override
 	public Object part1() {
-		return null;
+		in = getInputList().stream().map(x -> Integer.valueOf(x)).collect(Collectors.toList());
+		int count = 0;
+		for (int i = 1; i < in.size(); i++) {
+			if (in.get(i - 1) < in.get(i)) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	@Override
 	public Object part2() {
-		System.out.println(this.getInputString());
-		return null;
+		int count = 0;
+		int prevSum = in.get(0) + in.get(1) + in.get(2);
+		for (int i = 3; i < in.size(); i++) {
+			int newSum = prevSum - in.get(i - 3) + in.get(i);
+			if (prevSum < newSum) {
+				count++;
+			}
+			prevSum = newSum;
+		}
+		return count;
 	}
 
 }
