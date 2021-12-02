d458137366d2ac5ff00d1dd3ab7c1758eb422d85
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-12-02 07:22:07 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year21/Y21D02.java
+++ src/main/java/de/monx/aoc/year21/Y21D02.java
@@ -1,19 +1,64 @@
 package de.monx.aoc.year21;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.monx.aoc.util.Day;
 
 public class Y21D02 extends Day {
+	List<String> in = new ArrayList<>();
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		in = getInputList();
+		long horizontalPosition = 0;
+		long depth = 0;
+
+		for (var s : in) {
+			var sar = s.split(" ");
+			long x = Long.valueOf(sar[1]);
+			switch (sar[0]) {
+			case "forward":
+				horizontalPosition += x;
+				break;
+			case "down":
+				depth += x;
+				break;
+			case "up":
+				depth = Math.max(0, depth - x);
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + sar[0]);
+			}
+		}
+		return depth * horizontalPosition;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		long horizontalPosition = 0;
+		long aim = 0;
+		long depth = 0;
+
+		for (var s : in) {
+			var sar = s.split(" ");
+			long x = Long.valueOf(sar[1]);
+			switch (sar[0]) {
+			case "forward":
+				horizontalPosition += x;
+				depth = Math.max(0, depth + (x * aim));
+				break;
+			case "down":
+				aim += x;
+				break;
+			case "up":
+				aim -= x;
+				break;
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + sar[0]);
+			}
+		}
+		return depth * horizontalPosition;
 	}
 
 }
