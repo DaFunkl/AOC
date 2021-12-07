ff83f3761ce7d3beba8074f43a27ee3484ba3229
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-12-07 07:52:02 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year21/Y21D07.java
+++ src/main/java/de/monx/aoc/year21/Y21D07.java
@@ -1,19 +1,43 @@
 package de.monx.aoc.year21;
 
+import java.util.Arrays;
+import java.util.List;
+
 import de.monx.aoc.util.Day;
 
 public class Y21D07 extends Day {
+	List<Integer> in = Arrays.stream(getInputString().split(",")).map(Integer::valueOf).sorted().toList();
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		return solve(false);
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		return solve(true);
+	}
+
+	int solve(boolean p2) {
+		int minFuel = Integer.MAX_VALUE;
+		for (int i = in.get(0); i <= in.get(in.size() - 1); i++) {
+			int f = 0, x = i;
+			if (p2) {
+				f = in.stream().reduce(0, (a, b) -> a + dreieckszahl(Math.abs(b - x)));
+			} else {
+				f = in.stream().reduce(0, (a, b) -> a + Math.abs(b - x));
+			}
+			if (f < minFuel) {
+				minFuel = f;
+			} else if (f > minFuel) {
+				break;
+			}
+		}
+		return minFuel;
+	}
+
+	int dreieckszahl(int n) {
+		return (n * (n + 1)) / 2;
 	}
 
 }
