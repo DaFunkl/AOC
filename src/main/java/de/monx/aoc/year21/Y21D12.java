252be2f15f773cd196e4bd88f6c61ee34e6918f8
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-12-12 11:19:32 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year21/Y21D12.java
+++ src/main/java/de/monx/aoc/year21/Y21D12.java
@@ -25,23 +25,39 @@ public class Y21D12 extends Day {
 	}
 
 	int solve(boolean isPart2) {
+		// Stack to solve all paths,
+		// values = String[]
+		// idx 0: current Position
+		// idx 1: path
+		// idx 2: smallCave (Part2)
 		Stack<String[]> todos = new Stack<>();
+		// initial state = start, idx 1 can be left empty, doesn't matter, id 2 has to
+		// be null, since no small cave was visited twice
 		todos.push(new String[] { "start", "", null });
 		int ret = 0;
 		while (!todos.isEmpty()) {
-			var currentPath = todos.pop();
+			var currentPath = todos.pop(); // dfs - approach
+
+			// if a new node is pushed to stack, add nextNode to path (idx 1)
 			for (var nextNode : graph.get(currentPath[0])) { // end
+				// end was visited, increment return value, don't push to stack
 				if (nextNode.equals("end")) {
 					ret++;
 				} else if (nextNode.equals("start")) {
+					// start Node --> ignore
 					continue;
 				} else if (Util.isLoweCase(nextNode)) { // small cave
+					// small cave, only push small cave, if hasn't been seen before,
+					// or part2 and it's allowed to visit a small cave twice (idx 2 == null)
 					if (!currentPath[1].contains(nextNode)) {
+						// idx 2 = previous idx 2, since the path (idx 1) doesn't contain next Node
 						todos.push(new String[] { nextNode, currentPath[1] + nextNode, currentPath[2] });
 					} else if (isPart2 && currentPath[2] == null) {
+						// idx 2 has to be adjusted, next Node is contained in path (idx 1)
 						todos.push(new String[] { nextNode, currentPath[1] + nextNode, nextNode });
 					}
 				} else { // big cave
+					// big Cave, always push big caves, the are no cycles
 					todos.push(new String[] { nextNode, currentPath[1] + nextNode, currentPath[2] });
 				}
 			}
