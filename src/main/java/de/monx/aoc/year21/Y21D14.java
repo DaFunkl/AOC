a240411f421d543da9e6fe7312773d4bce1e6eb7
Alexey Gasevic <gasevic.alexey@gmail.com>
2021-12-15 12:59:59 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year21/Y21D14.java
+++ src/main/java/de/monx/aoc/year21/Y21D14.java
@@ -1,5 +1,6 @@
 package de.monx.aoc.year21;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,10 +9,10 @@ import de.monx.aoc.util.Day;
 
 public class Y21D14 extends Day {
 
-	String startString = "";
-	Map<String, Character> mapChar = new HashMap<>();
-	Map<String, Integer> mapIdx = new HashMap<>();
-	Map<Integer, String> mapKey = new HashMap<>();
+	List<String> in = getInputList();
+	long[] is = null;
+	long[] ic = null;
+	int[][] sm = null;
 
 	@Override
 	public Object part1() {
@@ -24,58 +25,67 @@ public class Y21D14 extends Day {
 		return solve(40);
 	}
 
-	long solve(int steps) {
-		Map<Character, Long> count = new HashMap<>();
-		long[] mapCount = new long[mapIdx.size()];
-		char[] car = startString.toCharArray();
-		for (int i = 1; i < car.length; i++) {
-			String k = "" + car[i - 1] + car[i];
-			mapCount[mapIdx.get(k)]++;
-			count.putIfAbsent(car[i - 1], 0l);
-			count.put(car[i - 1], count.get(car[i - 1]) + 1);
+	boolean init() {
+		Map<Character, Integer> charMap = new HashMap<>();
+		Map<String, Integer> nodeMap = new HashMap<>();
+		Map<String, Character> nextMap = new HashMap<>();
+		for (int i = 2; i < in.size(); i++) {
+			String str = in.get(i);
+			charMap.putIfAbsent(str.charAt(0), charMap.size());
+			charMap.putIfAbsent(str.charAt(1), charMap.size());
+			charMap.putIfAbsent(str.charAt(6), charMap.size());
+
+			String node = str.substring(0, 2);
+			nodeMap.put(node, nodeMap.size());
+			nextMap.put(node, str.charAt(6));
 		}
-		count.putIfAbsent(car[car.length - 1], 0l);
-		count.put(car[car.length - 1], count.get(car[car.length - 1]) + 1);
+
+		ic = new long[charMap.size()];
+		is = new long[nodeMap.size()];
+		sm = new int[nextMap.size()][3];
+		char[] car = getInputList().get(0).toCharArray();
+		ic[charMap.get(car[0])]++;
+		for (int i = 1; i < car.length; i++) {
+			ic[charMap.get(car[i])]++;
+			String node = "" + car[i - 1] + car[i];
+			is[nodeMap.get(node)]++;
+		}
+
+		for (var node : nextMap.keySet()) {
+			char nc = nextMap.get(node);
+			sm[nodeMap.get(node)][0] = charMap.get(nc);
+			sm[nodeMap.get(node)][1] = nodeMap.get("" + node.charAt(0) + nc);
+			sm[nodeMap.get(node)][2] = nodeMap.get("" + nc + node.charAt(1));
+		}
+		return true;
+	}
+
+	long solve(int steps) {
+
+		long[] is = Arrays.copyOf(this.is, this.is.length);
+		long[] ic = Arrays.copyOf(this.ic, this.ic.length);
+
 		for (int step = 0; step < steps; step++) {
-			long[] newMapCount = new long[mapIdx.size()];
-			for (int i = 0; i < mapCount.length; i++) {
-				if (mapCount[i] <= 0) {
+			long[] nis = new long[is.length];
+			for (int i = 0; i < is.length; i++) {
+				if (is[i] <= 0) {
 					continue;
 				}
-				String key = mapKey.get(i);
-				char nc = mapChar.get(key);
-
-				count.putIfAbsent(nc, 0l);
-				count.put(nc, count.get(nc) + mapCount[i]);
-
-				String nk1 = "" + key.charAt(0) + nc;
-				newMapCount[mapIdx.get(nk1)] += mapCount[i];
-				String nk2 = "" + nc + key.charAt(1);
-				newMapCount[mapIdx.get(nk2)] += mapCount[i];
+				ic[sm[i][0]] += is[i];
+				nis[sm[i][1]] += is[i];
+				nis[sm[i][2]] += is[i];
 			}
-			mapCount = newMapCount;
+			is = nis;
 		}
-		return solveCount(count);
+		return solveCount(ic);
 	}
 
-	long solveCount(Map<Character, Long> counter) {
+	long solveCount(long[] counter) {
 		long[] mm = { Long.MAX_VALUE, -1 };
-		for (var k : counter.keySet()) {
-			mm[0] = Math.min(mm[0], counter.get(k));
-			mm[1] = Math.max(mm[1], counter.get(k));
+		for (var k : counter) {
+			mm[0] = Math.min(mm[0], k);
+			mm[1] = Math.max(mm[1], k);
 		}
 		return mm[1] - mm[0];
-	}
-
-	void init() {
-		List<String> in = getInputList();
-		startString = in.get(0);
-		int count = 0;
-		for (int i = 2; i < in.size(); i++) {
-			var sar = in.get(i).split(" -> ");
-			mapChar.put(sar[0], sar[1].charAt(0));
-			mapKey.put(count, sar[0]);
-			mapIdx.put(sar[0], count++);
-		}
 	}
 }
