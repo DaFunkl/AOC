4408cfcd9592f38a3dced510e3c1c9262442a9d0
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-01-01 18:14:54 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year16/Y16D25.java
+++ src/main/java/de/monx/aoc/year16/Y16D25.java
@@ -4,16 +4,42 @@ import de.monx.aoc.util.Day;
 
 public class Y16D25 extends Day {
 
+	int[] nums = init();
+
 	@Override
 	public Object part1() {
-		
-		return null;
+		int adder = nums[0] * nums[1];
+		int n = 0;
+		while (!finished(adder, n)) {
+			n++;
+		}
+		return n;
+	}
+
+	boolean finished(int adder, int n) {
+		String str = Integer.toBinaryString(adder + n);
+		boolean zero = true;
+		for (int i = str.length() - 1; i >= 0; i--) {
+			if (zero && str.charAt(i) != '0') {
+				return false;
+			} else if (!zero && str.charAt(i) != '1') {
+				return false;
+			}
+			zero = !zero;
+		}
+		return true;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		return "it's done ´°`-´°`";
 	}
 
+	int[] init() {
+		var in = getInputList();
+		return new int[] { //
+				Integer.valueOf(in.get(1).split(" ")[1]), //
+				Integer.valueOf(in.get(2).split(" ")[1]), //
+		};
+	}
 }
