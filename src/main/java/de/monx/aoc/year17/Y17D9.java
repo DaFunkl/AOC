0c30976dd0adc8e8d9a1370240825e67acf60056
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-01-02 14:35:16 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year17/Y17D9.java
+++ src/main/java/de/monx/aoc/year17/Y17D9.java
@@ -3,17 +3,56 @@ package de.monx.aoc.year17;
 import de.monx.aoc.util.Day;
 
 public class Y17D9 extends Day {
+	String in = getInputString();
+	int ret1, ret2;
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		solve();
+		return ret1;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		return ret2;
+	}
+
+	void solve() {
+		ret1 = 0;
+		ret2 = 0;
+		int glvl = 0;
+		boolean garbage = false;
+		boolean ignore = false;
+		for (char c : in.toCharArray()) {
+			if (ignore) {
+				ignore = false;
+				continue;
+			}
+			if (c == '!') {
+				ignore = true;
+				continue;
+			}
+			if (garbage) {
+				if (c == '>') {
+					garbage = false;
+				} else {
+					ret2++;
+				}
+			} else {
+				switch (c) {
+				case '{':
+					glvl++;
+					ret1 += glvl;
+					break;
+				case '}':
+					glvl--;
+					break;
+				case '<':
+					garbage = true;
+					break;
+				}
+			}
+		}
 	}
 
 }
