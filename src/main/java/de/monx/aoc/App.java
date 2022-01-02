6b15a41d472bfc435c3914c207104821f40fb731
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-01-02 22:26:26 +0100

    quicksafe

--- src/main/java/de/monx/aoc/App.java
+++ src/main/java/de/monx/aoc/App.java
@@ -10,7 +10,7 @@ import de.monx.aoc.year21.Y21;
 
 public class App {
 	static int year = 17;
-	static int day = 10;
+	static int day = 11;
 
 	public static void main(String[] args) {
 		System.out.println("Year: " + year + ", Day: " + day);
--- src/main/java/de/monx/aoc/year17/Y17D11.java
+++ src/main/java/de/monx/aoc/year17/Y17D11.java
@@ -1,19 +1,43 @@
 package de.monx.aoc.year17;
 
 import de.monx.aoc.util.Day;
+import de.monx.aoc.util.common.pairs.IntPair;
 
 public class Y17D11 extends Day {
+	String[] in = getInputString().split(",");
+	int ret1, ret2;
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		solve();
+		return ret1;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		return ret2;
 	}
 
+	void solve() {
+		IntPair cp = new IntPair(0, 0);
+		int maxSecond = 0;
+		for (var s : in) {
+			cp.addi(dir(s));
+			maxSecond = Math.max(maxSecond, Math.abs(cp.second));
+		}
+		ret2 = maxSecond;
+		ret1 = cp.second;
+	}
+
+	IntPair dir(String str) {
+		return switch (str) {
+		case "n" -> new IntPair(-2, 0);
+		case "s" -> new IntPair(2, 0);
+		case "nw" -> new IntPair(1, -1);
+		case "sw" -> new IntPair(-1, -1);
+		case "ne" -> new IntPair(1, 1);
+		case "se" -> new IntPair(-1, 1);
+		default -> null;
+		};
+	}
 }
