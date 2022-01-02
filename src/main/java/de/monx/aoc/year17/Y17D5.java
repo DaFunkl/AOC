c0db0be7e63001734e30304b64670574ef8c158f
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-01-02 09:01:13 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year17/Y17D5.java
+++ src/main/java/de/monx/aoc/year17/Y17D5.java
@@ -1,19 +1,38 @@
 package de.monx.aoc.year17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import de.monx.aoc.util.Day;
 
 public class Y17D5 extends Day {
+	List<Integer> in = getInputList().stream().map(Integer::valueOf).toList();
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		var in = new ArrayList<>(this.in);
+		int ip = 0;
+		int stepper = 0;
+		while (0 <= ip && ip < in.size()) {
+			in.set(ip, in.get(ip) + 1);
+			ip += in.get(ip) - 1;
+			stepper++;
+		}
+		return stepper;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		var in = new ArrayList<>(this.in);
+		int ip = 0;
+		int stepper = 0;
+		while (0 <= ip && ip < in.size()) {
+			int offset = in.get(ip);
+			int adder = offset > 2 ? -1 : 1;
+			in.set(ip, offset + adder);
+			ip += in.get(ip) - adder;
+			stepper++;
+		}
+		return stepper;
 	}
-
 }
--- src/main/java/de/monx/aoc/year17/Y17D6.java
+++ src/main/java/de/monx/aoc/year17/Y17D6.java
@@ -1,19 +1,58 @@
 package de.monx.aoc.year17;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import de.monx.aoc.util.Day;
 
 public class Y17D6 extends Day {
+	int[] in = init();
+	int ret1, ret2;
 
 	@Override
 	public Object part1() {
-		
-		return null;
+		solve();
+		return ret1;
 	}
 
 	@Override
 	public Object part2() {
-		
-		return null;
+		return ret2;
 	}
 
+	void solve() {
+		Map<String, Integer> seen = new HashMap<>();
+		ret1 = 0;
+		int[] state = Arrays.copyOf(in, in.length);
+		String sk = Arrays.toString(state);
+		while (!seen.containsKey(sk)) {
+			seen.put(sk, ret1);
+			ret1++;
+			int max = 0;
+			int idx = 0;
+			for (int i = 0; i < state.length; i++) {
+				if (max < state[i]) {
+					max = state[i];
+					idx = i;
+				}
+			}
+			state[idx] = 0;
+			for (int i = 1; i <= max; i++) {
+				state[(i + idx) % state.length]++;
+			}
+			sk = Arrays.toString(state);
+		}
+		ret2 = ret1 - seen.get(sk);
+	}
+
+	int[] init() {
+		var in = getInputString();
+		var sar = in.split("\t");
+		int[] ret = new int[sar.length];
+		for (int i = 0; i < ret.length; i++) {
+			ret[i] = Integer.valueOf(sar[i]);
+		}
+		return ret;
+	}
 }
