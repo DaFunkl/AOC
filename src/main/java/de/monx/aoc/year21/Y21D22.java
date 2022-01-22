3cf662ca8f8138c73cf0415b99197803c7cc222b
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-01-22 09:28:37 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year21/Y21D22.java
+++ src/main/java/de/monx/aoc/year21/Y21D22.java
@@ -1,8 +1,7 @@
+
 package de.monx.aoc.year21;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 import de.monx.aoc.util.Day;
@@ -16,7 +15,7 @@ public class Y21D22 extends Day {
 
 	@Override
 	public Object part1() {
-		solve();
+		solve2();
 		return p1;
 	}
 
@@ -25,69 +24,44 @@ public class Y21D22 extends Day {
 		return p2;
 	}
 
-	void solve() {
+	void solve2() {
 		init();
-
-		List<long[][]> ons = new ArrayList<>();
-
-		long[][] irange = { //
-				{ -50, 50 }, //
-				{ -50, 50 }, //
-				{ -50, 50 } //
-		};
-		for (var opp : ops) {
-//			System.out.println(cc++ + "/" + (ops.size() - 1));
-			Deque<Pair<Boolean, long[][]>> stack = new ArrayDeque<>();
-			stack.push(opp);
-			while (!stack.isEmpty()) {
-				var op = stack.pop();
-				long[][] opCube = null;
-				opCube = op.second;
-				if (opCube == null) {
+		p1 = 0;
+		p2 = 0;
+		List<Pair<Boolean, long[][]>> cops = new ArrayList<>();
+		for (int i = 0; i < ops.size(); i++) {
+			var op = ops.get(i);
+			int mul = 1;
+			int cap = cops.size();
+			for (int j = 0; j < cap; j++) {
+				var cop = cops.get(j);
+				var inter = intersect(op.second, cop.second);
+				if (inter == null) {
 					continue;
 				}
-				int endIter = ons.size();
-				for (int i = 0; i < endIter; i++) {
-					var on = ons.get(i);
-					long[][] intersect = intersect(opCube, on);
-					if (intersect == null) {
-						continue;
-					} else {
+				var ncop = new Pair<Boolean, long[][]> //
+				(!cop.first, inter);
 
-						if (!op.first) {
-							ons.remove(i);
-							endIter--;
-							i--;
-							if (!same(on, intersect)) {
-								var onCut = cut(on, intersect);
-
-								ons.add(onCut.get(0));
-								for (int c = 1; c < onCut.size(); c++) {
-									ons.add(onCut.get(c));
-								}
-							}
-						}
-						if (same(opCube, intersect)) {
-							opCube = null;
-							break;
-						}
-						var cuts = cut(opCube, intersect);
-						opCube = cuts.get(0);
-						for (int c = 1; c < cuts.size(); c++) {
-							stack.push(new Pair<Boolean, long[][]>(op.first, cuts.get(c)));
-						}
-
-					}
-				}
-				if (opCube != null && op.first) {
-					ons.add(opCube);
-				}
-//				merge(ons);
+				mul = ncop.first ? 1 : -1;
+				p1 += mul * cubes(intersect(ncop.second, irange));
+				p2 += mul * cubes(ncop.second);
+				cops.add(ncop);
 			}
+			if (!op.first) {
+				continue;
+			}
+			mul = op.first ? 1 : -1;
+			p1 += mul * cubes(intersect(op.second, irange));
+			p2 += mul * cubes(op.second);
+			cops.add(op);
 		}
-		p1 = ons.stream().map(x -> cubes(intersect(x, irange))).reduce(Long::sum).get();
-		p2 = ons.stream().map(x -> cubes(x)).reduce(Long::sum).get();
 	}
+
+	long[][] irange = { //
+			{ -50, 50 }, //
+			{ -50, 50 }, //
+			{ -50, 50 } //
+	};
 
 	long cubes(long[][] arr) {
 		if (arr == null) {
@@ -111,152 +85,7 @@ public class Y21D22 extends Day {
 		return ret;
 	}
 
-	void merge(List<long[][]> in) {
-		boolean changed = true;
-		while (changed) {
-			changed = false;
-			for (int i = 0; i < in.size(); i++) {
-				var a = in.get(i);
-				for (int j = i + 1; j < in.size(); j++) {
-					var b = in.get(j);
-					if (isInside(a, b)) {
-						in.remove(j);
-						i--;
-						changed = true;
-						break;
-					}
-					if (isInside(b, a)) {
-						in.remove(i);
-						i--;
-						changed = true;
-						break;
-					}
-					var append = tryAppend(a, b);
-					if (append != null) {
-						in.set(i, append);
-						in.remove(j);
-						i--;
-						changed = true;
-						break;
-					}
-					append = tryAppend(b, a);
-					if (append != null) {
-						in.set(i, append);
-						in.remove(j);
-						i--;
-						changed = true;
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	long[][] tryAppend(long[][] a, long[][] b) {
-		boolean xe = a[0][0] == b[0][0] && a[0][1] == b[0][1];
-		boolean ye = a[1][0] == b[1][0] && a[1][1] == b[1][1];
-		boolean ze = a[2][0] == b[2][0] && a[2][1] == b[2][1];
-		if (xe && ye && a[2][0] <= b[2][0] && b[2][0] <= a[2][1] + 1) {
-			return new long[][] { //
-					{ a[0][0], a[0][1] }, //
-					{ a[1][0], a[1][1] }, //
-					{ a[2][0], b[2][1] },//
-			};
-		}
-		if (ye && ze && a[0][0] <= b[0][0] && b[0][0] <= a[0][1] + 1) {
-			return new long[][] { //
-					{ a[0][0], b[0][1] }, //
-					{ a[1][0], a[1][1] }, //
-					{ a[2][0], a[2][1] }, //
-			};
-		}
-		if (ze && xe && a[1][0] <= b[1][0] && b[1][0] <= a[1][1] + 1) {
-			return new long[][] { //
-					{ a[0][0], a[0][1] }, //
-					{ a[1][0], b[1][1] }, //
-					{ a[2][0], a[2][1] }, //
-			};
-		}
-		return null;
-	}
-
-	boolean isInside(long[][] a, long[][] b) {
-		for (int i = 0; i < 3; i++) {
-			if (!(a[i][0] <= b[i][0] && b[i][1] <= a[i][1])) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	long[] fetchMM(long[][] a, long[][] b, int i, int d) {
-		return switch (i) {
-		case 0 -> new long[] { //
-				a[d][0], // --> always a 0
-				Math.max(a[d][0], (b[d][0] - 1)) //
-			};
-		case 1 -> new long[] { //
-				b[d][0], // --> always d0
-				b[d][1] // --> always d1
-			};
-		case 2 -> new long[] { //
-				Math.min(a[d][1], (b[d][1] + 1)), //
-				a[d][1] // --> always a 1
-			};
-		default -> null;
-		};
-	}
-
-	List<long[][]> cut(long[][] a, long[][] b) {
-		List<long[][]> ret = new ArrayList<>();
-		if (same(a, b)) {
-			return null;
-		}
-
-		for (int z = 0; z < 3; z++) {
-			long[] zz = fetchMM(a, b, z, 2);
-			for (int y = 0; y < 3; y++) {
-				long[] yy = fetchMM(a, b, y, 1);
-				for (int x = 0; x < 3; x++) {
-					if (z == 1 && y == 1 && x == 1) {
-						continue;
-					}
-					long[] xx = fetchMM(a, b, x, 0);
-
-					long[][] cut = { { xx[0], xx[1] }, { yy[0], yy[1] }, { zz[0], zz[1] } };
-
-					boolean ignoreCut = false;
-					if (intersect(b, cut) != null) {
-						ignoreCut = true;
-					} else {
-						for (var r : ret) {
-							if (same(r, cut)) {
-								ignoreCut = true;
-								break;
-							}
-						}
-					}
-					if (!ignoreCut) {
-						ret.add(cut);
-					}
-				}
-			}
-		}
-		merge(ret);
-		return ret;
-	}
-
-	boolean same(long[][] a, long[][] b) {
-		for (int i = 0; i < 3; i++) {
-			if (a[i][0] != b[i][0] || a[i][1] != b[i][1]) {
-				return false;
-			}
-		}
-		return true;
-	}
-
 	void init() {
-
 		for (String s : in) {
 			boolean on = s.startsWith("on");
 			var xyz = s.split(" ")[1].replaceAll("[xyz=]", "").replace("..", ",").split(",");
