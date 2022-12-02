0f1fd18871184921c56903d56009563d829d61c7
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-12-02 08:24:20 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year22/Y22D02.java
+++ src/main/java/de/monx/aoc/year22/Y22D02.java
@@ -1,17 +1,39 @@
 package de.monx.aoc.year22;
 
+import java.util.List;
+
 import de.monx.aoc.util.Day;
+import de.monx.aoc.util.common.pairs.IntPair;
 
 public class Y22D02 extends Day {
 
+	List<IntPair> in = getInputList().stream() //
+			.map(x -> x.split(" ")) //
+			.map(x -> new IntPair( //
+					(int) x[0].charAt(0) - (int) 'A', //
+					(int) x[1].charAt(0) - (int) 'X'//
+			)) //
+			.toList();
+
 	@Override
 	public Object part1() {
-		return null;
+		int ret = 0;
+		for (var i : in) {
+			ret += i.second + 1;
+			if (i.first == i.second) {
+				ret += 3;
+			} else if (i.second - i.first == 1 || (i.second == 0 && i.first == 2)) {
+				ret += 6;
+			}
+		}
+		return ret;
 	}
 
 	@Override
 	public Object part2() {
-		return null;
+		return in.stream() //
+				.map(i -> i.second * 3 + 1 + (i.first + ((i.second + 2) % 3)) % 3) //
+				.reduce(0, (a, b) -> a + b);
 	}
 
 }
