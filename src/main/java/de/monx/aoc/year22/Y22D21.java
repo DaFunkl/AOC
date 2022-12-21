0c069a2076d0cc83d4792c502694c62eaf075a39
Alexey Gasevic <gasevic.alexey@gmail.com>
2022-12-21 09:32:51 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year22/Y22D21.java
+++ src/main/java/de/monx/aoc/year22/Y22D21.java
@@ -8,84 +8,65 @@ import de.monx.aoc.util.Util;
 import lombok.Data;
 
 public class Y22D21 extends Day {
+	Map<String, Monkey> monks1 = init();
+	Map<String, Monkey> monks2 = init();
 
 	@Override
 	public Object part1() {
-		Map<String, Monkey> monks = init();
-		while (monks.get("root").waiting) {
-			for (var k : monks.keySet()) {
-				var monk = monks.get(k);
-				if (!monk.waiting) {
-					continue;
-				}
-				var monk1 = monks.get(monk.name1);
-				if (monk1.waiting) {
-					continue;
-				}
-				var monk2 = monks.get(monk.name2);
-				if (monk2.waiting) {
-					continue;
-				}
-				monk.operate(monk1.result, monk2.result);
-			}
+		return fetchValue(monks1, "root").result;
+	}
+
+	Monkey fetchValue(Map<String, Monkey> monks, String key) {
+		var monk = monks.get(key);
+		if (!monk.waiting) {
+			return monk;
+		}
+		monk.operate(fetchValue(monks, monk.name1).result, fetchValue(monks, monk.name2).result);
+		return monk;
+	}
+
+	Monkey fetchValue2(Map<String, Monkey> monks, String key) {
+		var monk = monks.get(key);
+		if (!monk.waiting || key.equals("humn")) {
+			return monk;
 		}
 
-		return monks.get("root").result;
+		var monk1 = fetchValue2(monks, monk.name1);
+		var monk2 = fetchValue2(monks, monk.name2);
+
+		if (!monk1.waiting) {
+			monk.w1 = false;
+			monk.res1 = monk1.result;
+		}
+		if (!monk2.waiting) {
+			monk.w2 = false;
+			monk.res2 = monk2.result;
+		}
+		if (!monk.isWaiting()) {
+			monk.operate(fetchValue(monks, monk.name1).result, fetchValue(monks, monk.name2).result);
+		}
+		return monk;
 	}
 
 	@Override
 	public Object part2() {
-		Map<String, Monkey> monks = init();
-		monks.get("root").operator = "=";
-		var monk = monks.get("humn");
+//		Map<String, Monkey> monks2 = init();
+		monks2.get("root").operator = "=";
+		var monk = monks2.get("humn");
 		monk.name1 = "X";
 		monk.name2 = "X";
 		monk.operator = "X";
 		monk.waiting = true;
-		boolean changed = true;
-		while (changed) {
-			changed = false;
-			for (var k : monks.keySet()) {
-				monk = monks.get(k);
-				if (!monk.waiting || monk.name.equals("humn")) {
-					continue;
-				}
-				var monk1 = monks.get(monk.name1);
-				var monk2 = monks.get(monk.name2);
-
-				if (!monk1.waiting) {
-					monk.w1 = false;
-					monk.res1 = monk1.result;
-				}
-				if (!monk2.waiting) {
-					monk.w2 = false;
-					monk.res2 = monk2.result;
-				}
-				if (!monk.isWaiting()) {
-					monk.operate(monk1.result, monk2.result);
-					changed = true;
-				}
-			}
-		}
-		monk = monks.get("root");
+		monk = fetchValue2(monks2, "root");
 		long y = 0;
 		while (!monk.name.equals("humn")) {
 			long num1 = y;
 			long num2 = monk.w1 ? monk.res2 : monk.res1;
-//			 x + 5 = 10 | - 5
-//			 5 + x = 10 | - 5
-//			 x - 10 = 2 | + 10 => 2 + 10 = 12
-//			 10 - x = 2 | + x - 2 => 10 - 2 <--- this case
 			if (monk.w2 && monk.operator.equals("-")) {
 				monk.operator = "+";
 				num1 = num2;
 				num2 = y;
 			}
-
-//			 x * 5 = 10 | / 5
-//			 5 * x = 10 | / 5
-//			 x / 10 = 2 | * 10 => 10 * 2
-//			 10 / x = 2 | * x / 2 => 10 / 2 <-- this case
 			if (monk.w2 && monk.operator.equals("/")) {
 				System.out.println("c2");
 				monk.operator = "*";
@@ -99,27 +80,9 @@ public class Y22D21 extends Day {
 			case "=" -> num2;
 			default -> Monkey.error(monk);
 			};
-			monk = monk.w1 ? monks.get(monk.name1) : monks.get(monk.name2);
+			monk = monk.w1 ? monks2.get(monk.name1) : monks2.get(monk.name2);
 		}
 		return y;
-	}
-
-	String formular(Map<String, Monkey> map, String key) {
-		StringBuilder sb = new StringBuilder();
-		if (!map.containsKey(key)) {
-			return key;
-		}
-		var monk = map.get(key);
-		if (monk.waiting) {
-			sb.append("(");
-			sb.append(formular(map, monk.name1));
-			sb.append(monk.operator);
-			sb.append(formular(map, monk.name2));
-			sb.append(")");
-		} else {
-			sb.append(monk.result);
-		}
-		return sb.toString();
 	}
 
 	Map<String, Monkey> init() {
