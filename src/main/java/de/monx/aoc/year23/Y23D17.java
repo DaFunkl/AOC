7f4c0fcfe9345b9d879e2ee2b8b64b7ec0c5e6e0
Alexey Gasevic <gasevic.alexey@gmail.com>
2023-12-17 07:37:55 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year23/Y23D17.java
+++ src/main/java/de/monx/aoc/year23/Y23D17.java
@@ -1,9 +1,10 @@
 package de.monx.aoc.year23;
 
-import java.util.ArrayDeque;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 import de.monx.aoc.util.Day;
 
@@ -32,15 +33,20 @@ public class Y23D17 extends Day {
 
 	int solve(int min, int max) {
 		Map<String, Integer> bw = new HashMap<>();
-		ArrayDeque<int[]> stack = new ArrayDeque<>();
-		stack.push(new int[] { 0, 0, 0, 0, 0 });
-		stack.push(new int[] { 0, 0, 1, 0, 0 });
+		PriorityQueue<int[]> st = new PriorityQueue<>(new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return o1[4] - o2[4];
+			}
+		});
+		st.add(new int[] { 0, 0, 0, 0, 0 });
+		st.add(new int[] { 0, 0, 1, 0, 0 });
 		int ret = Integer.MAX_VALUE;
 		int gy = map.length - 1;
 		int gx = map[0].length - 1;
-		while (!stack.isEmpty()) {
+		while (!st.isEmpty()) {
 
-			var cur = stack.pollLast();
+			var cur = st.poll();
 
 			String k = toKey(cur);
 			if (bw.containsKey(k) && bw.get(k) <= cur[4]) {
@@ -67,7 +73,7 @@ public class Y23D17 extends Day {
 					}
 					w += map[ny][nx];
 					if (i > min) {
-						stack.push(new int[] { ny, nx, nd, i, w });
+						st.add(new int[] { ny, nx, nd, i, w });
 					}
 				}
 			}
