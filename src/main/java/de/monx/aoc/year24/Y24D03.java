a132ccd0304b63df3790b31bdc549f9b8a2cfe01
Alexey Gasevic <gasevic.alexey@gmail.com>
2024-12-03 07:10:17 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year24/Y24D03.java
+++ src/main/java/de/monx/aoc/year24/Y24D03.java
@@ -4,14 +4,51 @@ import de.monx.aoc.util.Day;
 
 public class Y24D03 extends Day {
 
+	String in = getInputList().stream().reduce("", (a, b) -> a + b);
 
 	@Override
 	public Object part1() {
-		return null;
+		return solve(in);
 	}
 
 	@Override
 	public Object part2() {
-		return null;
+		var ars = in.split("don't()");
+		int sum = solve(ars[0]);
+		for (int i = 1; i < ars.length; i++) {
+			var arr = ars[i].split("do()");
+			for (int j = 1; j < arr.length; j++) {
+				sum += solve(arr[j]);
+			}
+		}
+		return sum;
 	}
+
+	int solve(String in) {
+		int sum = 0;
+		String[] ars = in.split("mul\\(");
+		for (var a : ars) {
+			if (!a.contains(")")) {
+				continue;
+			}
+			var str = a.split("\\)")[0];
+			int[] ns = { 0, 0 };
+			int nr = 0;
+			for (var c : str.toCharArray()) {
+				if (c == ',') {
+					nr++;
+				} else if (Character.getNumericValue(c) < 0) {
+					nr = -1;
+					break;
+				} else {
+					ns[nr] = (ns[nr] * 10) + Character.getNumericValue(c);
+				}
+			}
+			if (nr != -1) {
+				sum += ns[0] * ns[1];
+			}
+		}
+		return sum;
+	}
+
 }
