56818d277d820818ad2eca76678d3ea6c5e4edb5
Alexey Gasevic <gasevic.alexey@gmail.com>
2024-12-11 15:17:43 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year24/Y24D11.java
+++ src/main/java/de/monx/aoc/year24/Y24D11.java
@@ -1,17 +1,60 @@
 package de.monx.aoc.year24;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import de.monx.aoc.util.Day;
+import de.monx.aoc.util.common.pairs.LongPair;
 
 public class Y24D11 extends Day {
 
+	Map<LongPair, Long> memo = new HashMap<>();
+	String[] in = getInputString().split(" ");
 
 	@Override
 	public Object part1() {
-		return null;
+		return solve(25);
 	}
 
 	@Override
 	public Object part2() {
-		return null;
+		return solve(75);
+	}
+
+	long solve(long startVal) {
+		long ret = 0;
+		for (var s : in) {
+			ret += fetchAmt(new LongPair(Long.valueOf(s), startVal));
+		}
+		return ret;
+	}
+
+	long fetchAmt(LongPair lp) {
+		if (memo.containsKey(lp)) {
+			return memo.get(lp);
+		}
+		if (lp.second == 0l) {
+			return 1l;
+		} else {
+			if (lp.first == 0l) {
+				memo.put(lp, fetchAmt(lp.add(1, -1)));
+				return memo.get(lp);
+			} else if (("" + lp.first).length() % 2 == 0) {
+				String s = "" + lp.first;
+				long l = Long.valueOf(s.substring(0, s.length() / 2));
+				long r = Long.valueOf(s.substring(s.length() / 2));
+				var lnlp = lp.add(0, -1);
+				lnlp.first = l;
+				var rnlp = lp.add(0, -1);
+				rnlp.first = r;
+				memo.put(lp, fetchAmt(lnlp) + fetchAmt(rnlp));
+				return memo.get(lp);
+			} else {
+				var nlp = lp.add(0, -1);
+				nlp.first *= 2024;
+				memo.put(lp, fetchAmt(nlp));
+				return memo.get(lp);
+			}
+		}
 	}
 }
