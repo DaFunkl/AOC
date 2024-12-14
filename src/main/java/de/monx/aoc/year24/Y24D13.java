d10465a4fb2b934b4368f90824656a3a248eb3da
Alexey Gasevic <gasevic.alexey@gmail.com>
2024-12-14 11:05:04 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year24/Y24D13.java
+++ src/main/java/de/monx/aoc/year24/Y24D13.java
@@ -6,42 +6,44 @@ import java.util.List;
 import de.monx.aoc.util.Day;
 
 public class Y24D13 extends Day {
-
 	List<long[][]> in = init();
 
 	@Override
 	public Object part1() {
-		return in.stream().mapToLong(x -> cost(x, 0)).sum();
+		long ret = 0;
+		for (int i = 0; i < in.size(); i++) {
+			long cost = cost(in.get(i), 123l);
+			ret += cost;
+		}
+		return ret;
 	}
+
+	long adder = 10000000000000l;
 
 	@Override
 	public Object part2() {
-		System.out.println(Long.MAX_VALUE);
-		return in.stream().mapToLong(x -> cost(x, 10000000000000l)).sum();
+		long ret = 0;
+		for (int i = 0; i < in.size(); i++) {
+			in.get(i)[2][0] += adder;
+			in.get(i)[2][1] += adder;
+			long cost = cost(in.get(i), adder);
+			ret += cost;
+		}
+		return ret;
 	}
 
-	long cost(long[][] m, long adder) {
-		m[2][0] += adder;
-		m[2][1] += adder;
-		long ma = Math.min(m[2][0] / m[0][0], m[2][1] / m[0][1]);
-		long[] best = { Integer.MAX_VALUE - 1, -1, -1 };
-		for (long i = ma; i >= 0; i--) {
-			long[] dxy = { m[2][0] - i * m[0][0], m[2][1] - i * m[0][1] };
-			if (dxy[0] % m[1][0] != 0 || dxy[1] % m[1][1] != 0 || (dxy[0] / m[1][0] != dxy[1] / m[1][1])) {
-				continue;
-			}
-			long j = dxy[0] / m[1][0];
-			long score = i * 3 + j;
-			if (score < best[0]) {
-				best[0] = score;
-				best[1] = i;
-				best[2] = j;
-			}
+	long cost(long[][] m, long limit) {
+		long au = (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
+		long ad = (m[1][0] * m[0][1] - m[1][1] * m[0][0]);
+		long a = au / ad;
+		long bu = (m[0][0] * m[2][1] - m[0][1] * m[2][0]);
+		long bd = (m[0][0] * m[1][1] - m[0][1] * m[1][0]);
+		long b = bu / bd;
+
+		if (bu % bd == 0 && au % ad == 0 && 0 <= b && b <= limit && 0 <= a && a <= limit) {
+			return 3 * a + b;
 		}
-		if (best[1] == -1) {
-			return 0;
-		}
-		return best[0];
+		return 0l;
 	}
 
 	List<long[][]> init() {
