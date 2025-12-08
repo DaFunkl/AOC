bbd39c6e4a594b24a1901a1d644a1b5f8f392cea
Alexey Gasevic <gasevic.alexey@gmail.com>
2025-12-08 08:06:34 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year25/Y25D07.java
+++ src/main/java/de/monx/aoc/year25/Y25D07.java
@@ -2,10 +2,8 @@ package de.monx.aoc.year25;
 
 import java.util.ArrayDeque;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import de.monx.aoc.util.Day;
 import de.monx.aoc.util.common.Pair;
@@ -14,6 +12,8 @@ import de.monx.aoc.util.common.pairs.IntPair;
 public class Y25D07 extends Day {
 	List<char[]> in = getInputList().stream().map(String::toCharArray).toList();
 	IntPair ip = new IntPair(0, 0);
+	int result_1 = 0;
+	long result_2 = 1;
 
 	@Override
 	public Object part1() {
@@ -23,95 +23,49 @@ public class Y25D07 extends Day {
 				break;
 			}
 		}
-		ArrayDeque<IntPair> stack = new ArrayDeque<>();
-		stack.add(ip.clone());
-		int ret = 0;
-		Set<IntPair> seen = new HashSet<>();
+		ArrayDeque<Pair<IntPair, Long>> stack = new ArrayDeque<>();
+		stack.add(new Pair<>(ip, 1l));
 		while (!stack.isEmpty()) {
-			Set<IntPair> nps = new HashSet<>();
+			Map<IntPair, Long> newPointers = new HashMap<>();
 			while (!stack.isEmpty()) {
-				var ip = stack.pop();
-				seen.add(ip);
-				if (ip.first + 1 >= in.size()) {
+				var pair = stack.pop();
+				var pointer = pair.first;
+				if (pointer.first + 1 >= in.size()) {
 					continue;
 				}
-				var c = in.get(ip.first + 1)[ip.second];
+				var c = in.get(pointer.first + 1)[pointer.second];
 				if (c == '.') {
-					nps.add(ip.add(1, 0));
+					var newPointer = pointer.add(1, 0);
+					if (newPointers.containsKey(newPointer)) {
+						newPointers.put(newPointer, newPointers.get(newPointer) + pair.second);
+					} else {
+						newPointers.put(newPointer, pair.second);
+					}
 				} else if (c == '^') {
-					nps.add(ip.add(1, -1));
-					nps.add(ip.add(1, 1));
-					ret++;
-				} else {
-					System.err.println(ip + " -> " + c);
+					var newPointerLeft = pointer.add(1, -1);
+					if (newPointers.containsKey(newPointerLeft)) {
+						newPointers.put(newPointerLeft, newPointers.get(newPointerLeft) + pair.second);
+					} else {
+						newPointers.put(newPointerLeft, pair.second);
+					}
+
+					var newPointerRight = pointer.add(1, 1);
+					if (newPointers.containsKey(newPointerRight)) {
+						newPointers.put(newPointerRight, newPointers.get(newPointerRight) + pair.second);
+					} else {
+						newPointers.put(newPointerRight, pair.second);
+					}
+					result_2 += pair.second;
+					result_1++;
 				}
 			}
-			nps.forEach(stack::add);
+			newPointers.entrySet().forEach(x -> stack.add(new Pair<>(x.getKey(), x.getValue())));
 		}
-		return ret;
+		return result_1;
 	}
 
 	@Override
 	public Object part2() {
-		for (int i = 0; i < in.get(0).length; i++) {
-			if (in.get(0)[i] == 'S') {
-				this.ip.second = i;
-				break;
-			}
-		}
-		ArrayDeque<Pair<IntPair, Long>> stack = new ArrayDeque<>();
-		stack.add(new Pair<>(ip, 1l));
-		long ret = 1;
-		while (!stack.isEmpty()) {
-			Map<IntPair, Long> nps = new HashMap<>();
-			while (!stack.isEmpty()) {
-				var ipp = stack.pop();
-				var ip = ipp.first;
-				if (ip.first + 1 >= in.size()) {
-					continue;
-				}
-				var c = in.get(ip.first + 1)[ip.second];
-				if (c == '.') {
-					var nip = ip.add(1, 0);
-					if (nps.containsKey(nip)) {
-						nps.put(nip, nps.get(nip) + ipp.second);
-					} else {
-						nps.put(nip, ipp.second);
-					}
-				} else if (c == '^') {
-					var nipl = ip.add(1, -1);
-					var nipr = ip.add(1, 1);
-					if (nps.containsKey(nipl)) {
-						nps.put(nipl, nps.get(nipl) + ipp.second);
-					} else {
-						nps.put(nipl, ipp.second);
-					}
-					if (nps.containsKey(nipr)) {
-						nps.put(nipr, nps.get(nipr) + ipp.second);
-					} else {
-						nps.put(nipr, ipp.second);
-					}
-					ret += ipp.second;
-				} else {
-					System.err.println(ip + " -> " + ipp.second);
-				}
-			}
-			nps.entrySet().forEach(x -> stack.add(new Pair<>(x.getKey(), x.getValue())));
-		}
-		return ret;
-	}
-
-	void print(Set<IntPair> seen) {
-		IntPair ip = new IntPair(0, 0);
-		StringBuilder sb = new StringBuilder();
-		for (ip.first = 0; ip.first < in.size(); ip.first++) {
-			for (ip.second = 0; ip.second < in.get(0).length; ip.second++) {
-				var c = in.get(ip.first)[ip.second];
-				sb.append(c == '.' && seen.contains(ip) ? '|' : c);
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-		System.out.println();
+		return result_2;
 	}
 }
--- src/main/java/de/monx/aoc/year25/Y25D08.java
+++ src/main/java/de/monx/aoc/year25/Y25D08.java
@@ -1,16 +1,99 @@
 package de.monx.aoc.year25;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import de.monx.aoc.util.Day;
 
 public class Y25D08 extends Day {
 
+	List<long[]> in = getInputList().stream()//
+			.map(x -> x.split(",")) //
+			.map(x -> new long[] { //
+					Long.valueOf(x[0]), //
+					Long.valueOf(x[1]), //
+					Long.valueOf(x[2]) //
+			}) //
+			.toList();
+	List<Set<Integer>> cons = new ArrayList<>();
+	List<long[]> odis = new ArrayList<>();
+
 	@Override
 	public Object part1() {
-		return null;
+		for (int i = 0; i < in.size(); i++) {
+			var a = in.get(i);
+			for (int j = i + 1; j < in.size(); j++) {
+				var b = in.get(j);
+				long[] sols = { a[0] - b[0], a[1] - b[1], a[2] - b[2] };
+				sols[0] *= sols[0];
+				sols[1] *= sols[1];
+				sols[2] *= sols[2];
+				odis.add(new long[] { i, j, sols[0] + sols[1] + sols[2] });
+			}
+		}
+
+		odis.sort(new Comparator<long[]>() {
+			@Override
+			public int compare(long[] o1, long[] o2) {
+				return Long.compare(o1[2], o2[2]);
+			}
+		});
+
+		for (int consCount = 0; consCount < 1000; consCount++) {
+			doIt();
+		}
+		return cons.stream().mapToInt(Set::size).boxed().sorted(Collections.reverseOrder()) //
+				.limit(3).reduce(1, (a, b) -> a * b);
 	}
 
 	@Override
 	public Object part2() {
-		return null;
+		int[] sol = null;
+		while (cons.get(0).size() < in.size()) {
+			sol = doIt();
+		}
+		return in.get(sol[0])[0] * in.get(sol[1])[0];
+	}
+
+	int[] doIt() {
+		int[] con = { 0, 0, -1, -1 };
+		var ods = odis.get(0);
+		int i = (int) ods[0];
+		int j = (int) ods[1];
+		for (int ci = 0; ci < cons.size(); ci++) {
+			var cs = cons.get(ci);
+			if (cs.contains(i)) {
+				con[2] = ci;
+			}
+			if (cs.contains(j)) {
+				con[3] = ci;
+			}
+			if (con[2] != -1 && con[3] != -1) {
+				break;
+			}
+		}
+		if (con[2] == con[3] && con[2] != -1) {
+			odis.remove(0);
+			return null;
+		}
+		if (con[2] == -1 && con[3] != -1) {
+			con[2] = con[3];
+			con[3] = -1;
+		} else if (con[2] == -1 && con[2] == -1) {
+			cons.add(new HashSet<>());
+			con[2] = cons.size() - 1;
+		}
+		cons.get(con[2]).add(i);
+		cons.get(con[2]).add(j);
+		if (con[3] != -1) {
+			cons.get(con[2]).addAll(cons.get(con[3]));
+			cons.remove(con[3]);
+		}
+		odis.remove(0);
+		return new int[] { i, j };
 	}
 }
