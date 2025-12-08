574d436f9402ca7e845f368d3587885d0d759ec7
Alexey Gasevic <gasevic.alexey@gmail.com>
2025-12-08 18:22:35 +0100

    quicksafe

--- src/main/java/de/monx/aoc/year25/Y25D08.java
+++ src/main/java/de/monx/aoc/year25/Y25D08.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import de.monx.aoc.util.Day;
@@ -19,8 +20,13 @@ public class Y25D08 extends Day {
 					Long.valueOf(x[2]) //
 			}) //
 			.toList();
-	List<Set<Integer>> cons = new ArrayList<>();
-	List<long[]> odis = new ArrayList<>();
+	List<Set<Integer>> connections = new ArrayList<>();
+	PriorityQueue<long[]> queue = new PriorityQueue<>(new Comparator<long[]>() {
+		@Override
+		public int compare(long[] o1, long[] o2) {
+			return Long.compare(o1[2], o2[2]);
+		}
+	});
 
 	@Override
 	public Object part1() {
@@ -28,73 +34,54 @@ public class Y25D08 extends Day {
 			var a = in.get(i);
 			for (int j = i + 1; j < in.size(); j++) {
 				var b = in.get(j);
-				long[] sols = { a[0] - b[0], a[1] - b[1], a[2] - b[2] };
-				sols[0] *= sols[0];
-				sols[1] *= sols[1];
-				sols[2] *= sols[2];
-				odis.add(new long[] { i, j, sols[0] + sols[1] + sols[2] });
+				queue.add(new long[] { i, j, //
+						((a[0] - b[0]) * (a[0] - b[0])) // x
+								+ ((a[1] - b[1]) * (a[1] - b[1])) // y
+								+ ((a[2] - b[2]) * (a[2] - b[2])) });// z
 			}
 		}
-
-		odis.sort(new Comparator<long[]>() {
-			@Override
-			public int compare(long[] o1, long[] o2) {
-				return Long.compare(o1[2], o2[2]);
-			}
-		});
-
 		for (int consCount = 0; consCount < 1000; consCount++) {
 			doIt();
 		}
-		return cons.stream().mapToInt(Set::size).boxed().sorted(Collections.reverseOrder()) //
+		return connections.stream().mapToInt(Set::size).boxed().sorted(Collections.reverseOrder()) //
 				.limit(3).reduce(1, (a, b) -> a * b);
 	}
 
 	@Override
 	public Object part2() {
 		int[] sol = null;
-		while (cons.get(0).size() < in.size()) {
+		while (connections.get(0).size() < in.size()) {
 			sol = doIt();
 		}
 		return in.get(sol[0])[0] * in.get(sol[1])[0];
 	}
 
 	int[] doIt() {
-		var ods = odis.get(0);
-		int i = (int) ods[0];
-		int j = (int) ods[1];
-		int coni = -1;
-		int conj = -1;
-		for (int ci = 0; ci < cons.size(); ci++) {
-			var cs = cons.get(ci);
-			if (cs.contains(i)) {
-				coni = ci;
-			}
-			if (cs.contains(j)) {
-				conj = ci;
-			}
-			if (coni != -1 && conj != -1) {
-				break;
-			}
-		}
-		if (coni == conj && coni != -1) {
-			odis.remove(0);
+//		var ods = orderedDistances.get(0);
+		var ods = queue.poll();
+		int[] pt = { (int) ods[0], (int) ods[1], -1, -1 };
+		for (int ci = 0; ci < connections.size(); ci++) {
+			var cs = connections.get(ci); // @formatter:off
+			if (cs.contains(pt[0])) pt[2] = ci;
+			if (cs.contains(pt[1])) pt[3] = ci;
+			if (pt[2] != -1 && pt[3] != -1) break;
+		}// @formatter:on 
+		if (pt[2] == pt[3] && pt[2] != -1) {
 			return null;
 		}
-		if (coni == -1 && conj != -1) {
-			coni = conj;
-			conj = -1;
-		} else if (coni == -1 && coni == -1) {
-			cons.add(new HashSet<>());
-			coni = cons.size() - 1;
+		if (pt[2] == -1 && pt[3] != -1) {
+			pt[2] = pt[3];
+			pt[3] = -1;
+		} else if (pt[2] == -1 && pt[2] == -1) {
+			connections.add(new HashSet<>());
+			pt[2] = connections.size() - 1;
 		}
-		cons.get(coni).add(i);
-		cons.get(coni).add(j);
-		if (conj != -1) {
-			cons.get(coni).addAll(cons.get(conj));
-			cons.remove(conj);
+		connections.get(pt[2]).add(pt[0]);
+		connections.get(pt[2]).add(pt[1]);
+		if (pt[3] != -1) {
+			connections.get(pt[2]).addAll(connections.get(pt[3]));
+			connections.remove(pt[3]);
 		}
-		odis.remove(0);
-		return new int[] { i, j };
+		return new int[] { pt[0], pt[1] };
 	}
 }
